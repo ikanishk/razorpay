00bd7e74be30b1602e248b8242de6433478eba51
kanishk <kanishkpandey34@gmail.com>
2026-06-12 13:45:20 +0530

    changed mapsid

--- src/main/java/com/codingshuttle/razorpay/operations/entity/SettlementPayment.java
+++ src/main/java/com/codingshuttle/razorpay/operations/entity/SettlementPayment.java
@@ -1,7 +1,6 @@
 package com.codingshuttle.razorpay.operations.entity;
 
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "settlement_payment")
 public class SettlementPayment {
@@ -9,8 +8,9 @@ public class SettlementPayment {
     @EmbeddedId
     private SettlementPaymentId id;
 
-    @MapsId()
+    @MapsId("settlementId")   // ← specify the attribute name
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "settlement_id", nullable = false)
     private Settlement settlement;
 }
+
